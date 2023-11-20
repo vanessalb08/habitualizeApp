b055783d16c475361cf5ca5b3ee614d60644e9bc
vanessalb08 <vanessalb08@gmail.com>
2023-11-20 18:02:43 -0300

    Initial Commit

---
+++ Habitualize/HabitualizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitualizeApp.swift
+//  Habitualize
+//
+//  Created by Vanessa Lima De Barros on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitualizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
