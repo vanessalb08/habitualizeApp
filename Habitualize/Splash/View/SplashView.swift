09d0306fe78c43ca9476230a1edd977f95151d57
vanessalb08 <vanessalb08@gmail.com>
2023-11-20 20:45:06 -0300

    create splashView.

--- Habitualize/Splash/View/SplashView.swift
+++ Habitualize/Splash/View/SplashView.swift
@@ -5,4 +5,42 @@
 //  Created by Vanessa Lima De Barros on 20/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct SplashView: View {
+
+    @State var state: SplashUIState = .loading
+
+    var body: some View {
+        switch state {
+        case .loading:
+            loadingView()
+        case .goToSignInScreen:
+            Text("Carregar Login")
+        case .goToHomeScreen:
+            Text("Carregar Home")
+        case .error(let mensage):
+            Text("Erro: \(mensage)")
+        }
+    }
+}
+
+extension SplashView {
+    func loadingView() -> some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.gray)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+struct SplashView_Preview: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
